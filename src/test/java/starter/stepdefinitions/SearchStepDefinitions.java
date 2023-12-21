ad694639ad513ecc71d76a43bc86100770802f78
suman kumar <suman1ew09@gmail.com>
2023-12-21 20:48:34 +0530

    update project

--- src/test/java/starter/stepdefinitions/SearchStepDefinitions.java
+++ src/test/java/starter/stepdefinitions/SearchStepDefinitions.java
@@ -2,9 +2,12 @@ package starter.stepdefinitions;
 
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.restassured.response.Response;
+import net.serenitybdd.core.Serenity;
 import net.serenitybdd.rest.SerenityRest;
 import net.thucydides.core.annotations.Steps;
-import org.hamcrest.Matchers;
+import org.junit.Assert;
+
 
 import static net.serenitybdd.rest.SerenityRest.restAssuredThat;
 import static org.hamcrest.Matchers.contains;
@@ -14,14 +17,23 @@ public class SearchStepDefinitions {
     @Steps
     public CarsAPI carsAPI;
 
-    @When("he calls endpoint {string}")
-    public void heCallsEndpoint(String arg0) {
-        SerenityRest.given().get(arg0);
-    }
+    @Steps
+    public PastaApi pastaAPI;
 
-    @Then("he sees the results displayed for apple")
-    public void heSeesTheResultsDisplayedForApple() {
-        restAssuredThat(response -> response.statusCode(200));
+    @Steps
+    public FruitApi fruitApi;
+
+    @When("he calls endpoint {string}")
+    public void heCallsEndpoint(String resource) {
+        Response respone=SerenityRest.given().get(resource);
+        Serenity.setSessionVariable("respone").to(respone);
+        respone.prettyPrint();
+        if(resource.contains("pasta"))
+            Serenity.setSessionVariable("pastaRespone").to(respone);
+        else if(resource.contains("cola"))
+            Serenity.setSessionVariable("colaRespone").to(respone);
+        else
+            Serenity.setSessionVariable("respone").to(respone);
     }
 
     @Then("he sees the results displayed for mango")
@@ -31,6 +43,45 @@ public class SearchStepDefinitions {
 
     @Then("he doesn not see the results")
     public void he_Doesn_Not_See_The_Results() {
-        restAssuredThat(response -> response.body("error", contains("True")));
+        //restAssuredThat(response -> response.body("detail.error".g, contains("true".toString())));
+       Response response= Serenity.sessionVariableCalled("respone");
+        Assert.assertTrue(response.getBody().asString().contains("Not found"));
     }
+
+    @Then("he sees the results displayed for ([^\"]*)$")
+    public void heSeesTheResultsDisplayedForpasta(String item) {
+        restAssuredThat(response -> response.statusCode(200));
+
+    }
+    @Then("we validate the response of ([^\"]*)$")
+    public void validateTheResponse(String item) {
+        if(item.contains("pasta"))
+        {
+            Response response= Serenity.sessionVariableCalled("pastaRespone");
+            pastaAPI.VerifyImage(response);
+        }
+
+    }
+    @Then("response should contains ([^\"]*) of ([^\"]*)$")
+    public void responseValidation(String validation,String item) {
+        if(item.contains("pasta"))
+        {
+            Response response= Serenity.sessionVariableCalled("pastaRespone");
+            response.getBody().asString().contains(item);
+        }
+
+    }
+
+    @When("he calls endpoint {string} using post method")
+    public void callendpointPost(String resource) {
+        Response respone=SerenityRest.given().post(resource);
+        respone.prettyPrint();
+        if(resource.contains("pasta"))
+            Serenity.setSessionVariable("pastaRespone").to(respone);
+        else if(resource.contains("cola"))
+            Serenity.setSessionVariable("colaRespone").to(respone);
+        else
+            Serenity.setSessionVariable("respone").to(respone);
+    }
+
 }
--- src/test/resources/features/search/post_product.feature
+++ src/test/resources/features/search/post_product.feature
@@ -3,11 +3,25 @@ Feature: Search for the product
 ### Please use endpoint GET https://waarkoop-server.herokuapp.com/api/v1/search/demo/{product} for getting the products.
 ### Available products: "orange", "apple", "pasta", "cola"
 ### Prepare Positive and negative scenarios
+  @pasta_positive
+  Scenario: validating the positive scenario
+    When he calls endpoint "https://waarkoop-server.herokuapp.com/api/v1/search/demo/pasta"
+    Then he sees the results displayed for pasta
+    Then we validate the response of pasta
 
-  Scenario:
-    When he calls endpoint "https://waarkoop-server.herokuapp.com/api/v1/search/demo/orange"
-    Then he sees the results displayed for apple
-    When he calls endpoint "https://waarkoop-server.herokuapp.com/api/v1/search/demo/apple"
-    Then he sees the results displayed for mango
+  @pasta_negative-01
+  Scenario: validating the positive scenario with invalid resource
+    When he calls endpoint "https://waarkoop-server.herokuapp.com/api/v1/search/demo/pasta/01"
+    Then response should contains Not Found of pasta
+
+  @pasta_negative-02
+  Scenario: validating the positive scenario with invalid resource using post method
+    When he calls endpoint "https://waarkoop-server.herokuapp.com/api/v1/search/demo/pasta" using post method
+    Then response should contains Method Not Allowed of pasta
+
+  @positive
+  Scenario: validating the positive scenario for all product
+    When he calls endpoint "https://waarkoop-server.herokuapp.com/api/v1/search/demo/cola"
+    Then he sees the results displayed for cola
     When he calls endpoint "https://waarkoop-server.herokuapp.com/api/v1/search/demo/car"
     Then he doesn not see the results
